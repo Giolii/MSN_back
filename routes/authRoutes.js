b8dbc4b65c8f515c54109bed848233f39af7b67e
Gi <160989051+Giolii@users.noreply.github.com>
2025-03-25 20:54:05 -0700

    Delete old comments

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,14 +7,6 @@ const authenticateJWT = passport.authenticate("jwt", { session: false });
 router.post("/register", authController.signup);
 router.post("/login", authController.login);
 router.get("/me", authenticateJWT, authController.authenticateMe);
-// Login Guest
 router.post("/guest", authController.guest);
-
-// User route
-// router.get("/users", authenticateJWT, authController.allUsers);
-// router.post("/update", authenticateJWT, authController.updateProfile);
-// router.get("/:username", authenticateJWT, authController.checkUsername);
-// router.post("/addFriend", authenticateJWT, authController.addFriend);
-// router.post("/removeFriend", authenticateJWT, authController.removeFriend);
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,7 @@ const passport = require("passport");
 const authenticateJWT = passport.authenticate("jwt", { session: false });
 
 router.get("/all", authenticateJWT, userController.allUsers);
+router.post("/admin", authenticateJWT, userController.becomeAdmin);
 router.post("/update", authenticateJWT, userController.updateProfile);
 router.post("/addFriend", authenticateJWT, userController.addFriend);
 router.post("/removeFriend", authenticateJWT, userController.removeFriend);
